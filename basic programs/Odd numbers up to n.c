08b72ee9bb8d6bf5ee287915c2b7d4e2df529611
Mahesh Polineni <79160137+polinenim@users.noreply.github.com>
2022-02-14 22:46:56 +0530

    Odd numbers to find in c

---
+++ basic programs/Odd numbers up to n.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    int n,i;
+    printf("Enter a positive integer range: ");
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+		if(i%2!=0)
+		{
+			printf("THE ODD NUMBERS IS:%d\n",i);
+		}
+	}
+   return 0;
+}
