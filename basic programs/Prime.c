cccf127e026bf7080db09e60076b58bc2950b4b5
Mahesh Polineni <maheshpolineni1008@gmail.com>
2022-02-22 23:06:34 +0530

    Check whether the given number

---
+++ basic programs/Prime.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a,j,c=0;
+	printf("enter the number");
+	scanf("%d",&a);
+	for(j=1;j<=a;j++)
+	{
+		if(a%j==0)
+		{
+			c=c+1;
+		}
+	}
+	if(c==2)
+    {
+		printf("%d is a prime number\n",a);
+	}
+	if(c!=2)
+	{
+		printf("%d is not a prime number\n",a);
+	}
+	return 0;
+	
+}
