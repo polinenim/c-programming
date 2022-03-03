0e12ce50cabcce5043b76bc61addb0a3d41f9cce
Mahesh Polineni <maheshpolineni1008@gmail.com>
2022-03-03 23:17:04 +0530

    print the numbers in ascending order by using Selection sort technique

---
+++ basic programs/Selection sort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int n,t,i,j,min;
+	printf("enter the size of an array");
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		printf("enter the values ");
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		min=i;
+		for(j=i+1;j<n;j++)
+		{
+			if(a[j]<a[min])
+			{
+				min=j;
+			}
+		}
+		t=a[i];
+		a[i]=a[min];
+		a[min]=t;
+	}
+	printf("the values are:");
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	return 0;
+}
